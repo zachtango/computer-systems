41163647181f497638c71eaf0228e3ccabe2b14b
Zach Tang <zachtangoj@gmail.com>
2022-09-14 20:24:48 -0500

    data lab added

--- ch2/overflow.c
+++ ch2/overflow.c
@@ -24,13 +24,25 @@ int tadd_ok(int x, int y){
     return 0;
 }
 
+int isLessOrEqual(int x, int y) {
+  // x <= y if x - y <= 0 --> x - y (not >) 0
+  y = ~y + 1; // negate y
+
+  int s = 1 << 31; // 1000...000, nonegative anded with s --> 0, !!(negative anded s) is 1
+
+//   printf("%d", s & (x + y));
+
+  return !!( !(x + y) | (s & (x + y)) ); // is 0 or is negative 
+  
+}
+
 int main(){
+    // 0x30 <= x <= 0x39
+    for(int i = 0x30 - 1000; i <= 0x39 + 1000; i++){
+        // isAsciiDigit(i);
+        // isLessOrEqual(i, i + 1);
+        printf("%d", isLessOrEqual(i + 3, i + 2));
 
-    int overflow = tadd_ok(__INT_MAX__, __INT_MAX__);
-    int overflow2 = tadd_ok(-__INT_MAX__, -__INT_MAX__);
-    int ok = uadd_ok(0, 0);
-
-    printf("%d %d %d", overflow, overflow2, ok);
-
+    }
     return 0;
 }
