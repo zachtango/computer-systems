1a53f14af1f71c300a928003cbaea772cc89ea57
Zach Tang <zxt200009@cslinux1.utdallas.edu>
2022-10-31 19:26:29 -0500

    warmup a9 finished

--- a9/pipedata.c
+++ a9/pipedata.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 
 #define MAX_ARGS 15
+#define MAX_BUFFER_LEN 1000000
 
 int main(int argc, char *argv[]){
 
@@ -17,20 +18,25 @@ int main(int argc, char *argv[]){
 	char *command2[MAX_ARGS];
 
 	int i = 0;
-	char *token = strtok(command1, " ");
+	char *token = strtok(argv[1], " ");
 	while(token){
+		//printf("%s\n", token);
 		command1[i] = token;
+		token = strtok(NULL, " "); // continue detokenizing
+		
 		i += 1;
 	}
 	command1[i] = NULL;
 	i = 0;
 
-	token = strtok(command2, " ");
+	token = strtok(argv[2], " ");
 	while(token){
+		//printf("%s\n", token);
 		command2[i] = token;
+		token = strtok(NULL, " "); // continue detokenizing
+
 		i += 1;
 	}
-	command2[i] = NULL;
 
 	// pipe setup
 	int parent[2];
@@ -40,7 +46,7 @@ int main(int argc, char *argv[]){
 	if(fork() == 0){
 		// child
 
-		dup2(parent[1], 1);
+		dup2(parent[1], STDOUT_FILENO);
 
 		close(parent[0]);
 		close(parent[1]);
@@ -53,16 +59,29 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
+	dup2(parent[0], STDIN_FILENO);
+
 	wait(NULL);
 
 	close(parent[1]);
 
-	char buffer[10];
+	// debugging reading the output of command1
+	/*char buffer[MAX_BUFFER_LEN];
+	int j = 0;
 
-	read(parent[0], buffer, 1);
-	close(parent[0]);
+	while(read(parent[0], buffer + j, 1) > 0){
+		j += 1;
+	}
 
-	printf("%s\n", buffer);
+	command2[i] = buffer;
+	command2[i + 1] = NULL;
+	*/
+
+	close(parent[0]);	
+
+	execvp(command2[0], command2);
+
+	perror("execvp");
 
 	return 0;
 }
